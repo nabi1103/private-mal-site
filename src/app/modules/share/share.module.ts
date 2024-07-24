24c485244893c3c4d3e9820f66eafd4a2ddfac48
linhng <linh.nguyen@achtzig20.de>
2024-07-24 13:39:31 +0200

    add(share-module)

---
+++ src/app/modules/share/share.module.ts
@@ -0,0 +1,9 @@
+// Framework
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+    declarations: [],
+    imports: [CommonModule],
+})
+export class ShareModule {}
