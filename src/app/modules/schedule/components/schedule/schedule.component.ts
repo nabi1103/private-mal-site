ef17a3d37349de31655ba5eae85511cf341fec16
linhng <linh.nguyen@achtzig20.de>
2024-07-25 10:28:23 +0200

    add(schedule module): init schedule module

---
+++ src/app/modules/schedule/components/schedule/schedule.component.ts
@@ -0,0 +1,9 @@
+// Framework
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-schedule",
+    templateUrl: "./schedule.component.html",
+    styleUrls: ["./schedule.component.scss"],
+})
+export class ScheduleComponent {}
---
+++ src/app/modules/schedule/schedule.module.ts
@@ -0,0 +1,17 @@
+// Framework
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ScheduleComponent } from './components/schedule/schedule.component';
+
+@NgModule({
+    declarations: [
+        ScheduleComponent,
+    ],
+    imports: [
+        CommonModule,
+    ],
+    exports: [
+        ScheduleComponent,
+    ]
+})
+export class ScheduleModule { }
