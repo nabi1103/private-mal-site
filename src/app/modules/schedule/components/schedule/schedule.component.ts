69742544f3ba6a14e896f2fa3c5d308984d2f611
linhng <linh.nguyen@achtzig20.de>
2024-07-25 13:22:11 +0200

    add(schedule module): implement layout

--- src/app/modules/schedule/components/schedule/schedule.component.ts
+++ src/app/modules/schedule/components/schedule/schedule.component.ts
@@ -1,6 +1,9 @@
 // Framework
-import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
+import { Component, EventEmitter, Input, OnChanges, Output } from "@angular/core";
 import { Observable, timer, map, shareReplay } from "rxjs";
+// Interface
+import { Anime } from "src/app/modules/share/interfaces/anime.interface";
+
 
 @Component({
     selector: "app-schedule",
@@ -9,29 +12,64 @@ import { Observable, timer, map, shareReplay } from "rxjs";
 })
 export class ScheduleComponent implements OnChanges {
     @Input() resource: any = null;
-    animeArray: Object[] = [];
+    @Output() onDeleteOneEntryClicked = new EventEmitter<string>();
+    @Output() onDeleteScheduleClicked = new EventEmitter<null>();
+    readonly week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    animeArray: Anime[] = [];
 
     private _time$: Observable<Date> = timer(0, 1000).pipe(
         map(tick => new Date()),
-        shareReplay(1)
+        shareReplay(1),
     );
 
     get time() {
         return this._time$;
     }
-    constructor() {}
 
     ngOnChanges(): void {
         this.formatScheduleData();
     }
 
-    formatScheduleData(): void {
+    getAnimeOnWeekday(day: string): Anime[] {
+        const tmp = this.animeArray.filter(
+            (a: Anime) => this.animeOnWeekday(a, day)
+        );
+        tmp.sort((a: Anime, b: Anime) => {
+            return this.compareHourString(a.broadcast.time, b.broadcast.time);
+        });
+        return tmp;
+    }
+
+    deleteEntry(id: number): void {
+        this.onDeleteOneEntryClicked.emit(id.toString());
+    }
+
+    deleteSchedule(): void {
+        this.onDeleteScheduleClicked.emit();
+    }
+
+    private formatScheduleData(): void {
         if (this.resource != null) {
+            this.animeArray = [];
             Object.values(this.resource).forEach(
                 (anime: any) => {
                     this.animeArray.push(JSON.parse(anime.toString()));
                 }
             );
+        }
+    }
+
+    private animeOnWeekday(anime : Anime, day: string): boolean {
+        return anime?.broadcast?.day == day + "s";
+    }
+
+    private compareHourString(hourA: string, hourB: string): number {
+        if (hourA > hourB) {
+            return 1;
+        } else if (hourA < hourB) {
+            return -1;
+        } else {
+            return 0;
         }
     }
 
@@ -46,6 +84,10 @@ export class ScheduleComponent implements OnChanges {
         }
 
         console.log(values);
+    }
+
+    test2(): void {
+        console.log("here");
     }
 
     clear(): void {
