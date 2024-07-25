9724d70c83879de36fe670edc5dcfab4524cfccd
linhng <linh.nguyen@achtzig20.de>
2024-07-25 11:32:22 +0200

    add(current-season.html, schedule.ts): embed schedule component

--- src/app/modules/schedule/components/schedule/schedule.component.ts
+++ src/app/modules/schedule/components/schedule/schedule.component.ts
@@ -1,5 +1,5 @@
 // Framework
-import { Component } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { Observable, timer, map, shareReplay } from "rxjs";
 
 @Component({
@@ -7,10 +7,9 @@ import { Observable, timer, map, shareReplay } from "rxjs";
     templateUrl: "./schedule.component.html",
     styleUrls: ["./schedule.component.scss"],
 })
-export class ScheduleComponent {
-    constructor() {
-
-    }
+export class ScheduleComponent implements OnChanges {
+    @Input() resource: any = null;
+    animeArray: Object[] = [];
 
     private _time$: Observable<Date> = timer(0, 1000).pipe(
         map(tick => new Date()),
@@ -19,5 +18,37 @@ export class ScheduleComponent {
 
     get time() {
         return this._time$;
+    }
+    constructor() {}
+
+    ngOnChanges(): void {
+        this.formatScheduleData();
+    }
+
+    formatScheduleData(): void {
+        if (this.resource != null) {
+            Object.values(this.resource).forEach(
+                (anime: any) => {
+                    this.animeArray.push(JSON.parse(anime.toString()));
+                }
+            );
+        }
+    }
+
+    // TODO
+    test(): void {
+        var values = [],
+            keys = Object.keys(localStorage),
+            i = keys.length;
+
+        while (i--) {
+            values.push(localStorage.getItem(keys[i]));
+        }
+
+        console.log(values);
+    }
+
+    clear(): void {
+        localStorage.clear();
     }
 }
